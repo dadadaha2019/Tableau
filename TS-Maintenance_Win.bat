47e5782fe19c6b75e674b04d3c80aa12af965f23
dadadaha2019 <78021585+dadadaha2019@users.noreply.github.com>
2021-07-30 17:04:08 +1000

    Update TS-Maintenance_Win.bat

--- TS-Maintenance_Win.bat
+++ TS-Maintenance_Win.bat
@@ -1,15 +1,17 @@
-@echo off
-set logfilename=%COMPUTERNAME%-logs
-set newlogfilename=%logfilename%_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%
-set backupfilename = %COMPUTERNAME%-backup
-set newbackupfilename=%backupfilename%_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%
+@ECHO off
+SET logfilename=%COMPUTERNAME%-logs
+SET newlogfilename=%logfilename%_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%
+SET backupfilename=%COMPUTERNAME%-backup
+SET newbackupfilename=%backupfilename%_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%
 
 ECHO === Starting Ziplog : %date% %time% ===
 call tsm maintenance ziplogs -f %logfilename% --overwrite
 ECHO === Finished Ziplog task ===
 
 ECHO === Moving ziplog file: %date% %time%===
+@ECHO on
 move /y "c:\ProgramData\Tableau\Tableau Server\data\tabsvc\files\log-archives\%logfilename%.zip" c:\tsbackup\%newlogfilename%.zip
+@ECHO off
 ECHO === Finished Moving ziplog task ===
 
 ECHO === Starting Claenup : %date% %time%  ===
@@ -21,5 +23,7 @@ call tsm maintenance backup -f %backupfilename%.tsbak
 ECHO === Finished Backup : %date% %time% ===
 
 ECHO == Moving Backup File : %date% %time% ===
-move /y "c:\ProgramData\Tableau\Tableau Server\data\tabsvc\files\backups\%COMPUTERNAME%.tsbak" c:\tsbackup\%newbackupfilename%.tsbak
+@ECHO on
+move /y "c:\ProgramData\Tableau\Tableau Server\data\tabsvc\files\backups\%backupfilename%.tsbak" c:\tsbackup\%newbackupfilename%.tsbak
+@ECHO off
 ECHO === Finishied Moving Backup file : %date% %time% ===
